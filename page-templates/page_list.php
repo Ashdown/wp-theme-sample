0bdcb93e96b06366e853d1be5ab776e006e7ed1d
Wordpress Development <wp_development@Rorys-MacBook-Pro.local>
2016-01-02 16:46:41 +0000

    Added page and page list templates

---
+++ page-templates/page_list.php
@@ -0,0 +1,9 @@
+<?php /* Template Name: List Template */ ?>
+
+<?php get_header(); ?>
+
+    <h1>List Page</h1>
+    <p>This is a sample item list page</p>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ page.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+    <h1>Item Page</h1>
+    <p>This is a sample item page</p>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
