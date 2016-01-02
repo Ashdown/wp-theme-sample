214e3629ee2bc9515c21185467a326f44eff23f1
Wordpress Development <wp_development@Rorys-MacBook-Pro.local>
2016-01-02 17:21:54 +0000

    added html tags to header and footer

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
-<h2>Footer Content</h2>
-<p>Thit is footer content</p>
-<ul>
-    <li>Footer Item</li>
-    <li>Footer Item</li>
-</ul>
+        <footer>
+            <h2>Footer Content</h2>
+            <p>This is footer content</p>
+            <ul>
+                <li>Footer Item</li>
+                <li>Footer Item</li>
+            </ul>
+        </footer>
+    </body>
+</html>
--- header.php
+++ header.php
@@ -1,5 +1,17 @@
-<ul>
-    <li>Header Item</li>
-    <li>Header Item</li>
-    <li>Header Item</li>
-</ul>
+<!DOCTYPE html>
+<html lang="en-GB">
+    <meta charset="UTF-8">
+
+    <head>
+        <title>Some page title</title>
+    </head>
+
+    <body>
+
+        <header>
+            <ul>
+                <li>Header Item</li>
+                <li>Header Item</li>
+                <li>Header Item</li>
+            </ul>
+        </header>
