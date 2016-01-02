30ae61d595a0bcaac1525b2ab20f62ea1512c4d9
Wordpress Development <wp_development@Rorys-MacBook-Pro.local>
2016-01-02 17:17:24 +0000

    added list of page links to page list template

--- page-templates/page_list.php
+++ page-templates/page_list.php
@@ -2,8 +2,12 @@
 
 <?php get_header(); ?>
 
-    <h1>List Page</h1>
-    <p>This is a sample item list page</p>
+<h1>List Page</h1>
+<p>This is a sample item list page</p>
+
+<ul>
+    <?php wp_list_pages('exclude=10&title_li='); ?>
+</ul>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
