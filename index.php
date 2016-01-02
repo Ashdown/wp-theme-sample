87f1e1c5573227238851145500f6274f84ef8326
Wordpress Development <wp_development@Rorys-MacBook-Pro.local>
2016-01-02 16:26:39 +0000

    changed content on homepage

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php get_header(); ?>
 
-<h1>This is the main page header</h1>
-<p>This is the main page description</p>
+<h1>Homepage</h1>
+<p>This is the homepage for the sample wordpress theme</p>
+<h2>Types of pages</h2>
+<p>There are three types of pages in this app:</p>
+<ul>
+    <li>The homepage</li>
+    <li>The item list page</li>
+    <li>The item page</li>
+</ul>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
