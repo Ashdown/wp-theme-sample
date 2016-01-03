a367ac82bb8014856ca5db177d82efeb6c78f34d
Wordpress Development <wp_development@Rorys-MacBook-Pro.local>
2016-01-03 16:23:03 +0000

    adding twitter bootstrap and jquery

--- footer.php
+++ footer.php
@@ -6,6 +6,8 @@
                 <li>Footer Item</li>
             </ul>
         </footer>
+        <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/js/lib/jquery.js"></script>
+        <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/js/lib/bootstrap.js"></script>
         <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/js/sample.js"></script>
         <?php wp_footer(); ?>
     </body>
